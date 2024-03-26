70efa6a0a1bd43bb4d0b0f1612dfa3fd6414478d
Jagdeep-Birdee <161628021+Jagdeep-Birdee@users.noreply.github.com>
2024-03-26 14:40:47 +0000

    added hamburger menu functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello! Welcome to my Portfolio')
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+
+
